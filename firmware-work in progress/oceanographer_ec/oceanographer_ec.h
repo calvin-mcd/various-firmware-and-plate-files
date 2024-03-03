9c22ac59cb3b7624991973d2be5b714b216ea028
calvin-mcd <114294748+calvin-mcd@users.noreply.github.com>
2024-03-03 17:30:19 +0100

    added oceanographer
    no oled yet

---
+++ firmware-work in progress/oceanographer_ec/oceanographer_ec.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "quantum.h"
+#include "ec_switch_matrix.h"
+
+#define XXX KC_NO
+
+enum ec_keycodes {
+    EC_AP_I = QK_KB_0,  // Increases actuation point (more travel)
+    EC_AP_D,            // Decreases actuation point
+    EC_CLR              // Reset EC config
+};
+
+// clang-format off
+//#define LAYOUT( \
+//	 k0A, k0B, k0C, k0D, k0E, k0F, K0G, k0H, k0I, k0J, k0K, k0L, k1A, k1B, k1C, k1D, k1E, k1F, k1G, k1H, k1I, k1J, k1K, k1L, k2A, k2B, k2C, k2D, k2E, k2F, k2G, k2H, k2I, k2J, k2K, k2L, k3A, k3B, k3C, k3D, k3E, k3F, k3H, k3I, k3J, k3K, k3L \
+//) { \
+//	{k0A, k0B, k0C, k0D, k0E, k0F, K0G, k0H, k0I, k0J, k0K, k0L}, \
+//	{k1A, k1B, k1C, k1D, k1E, k1F, k1G, k1H, k1I, k1J, k1K, k1L}, \
+//	{k2A, k2B, k2C, k2D, k2E, k2F, k2G, k2H, k2I, k2J, k2K, k2L}, \
+//	{k3A, k3B, k3C, k3D, k3E, k3F, XXX, k3H, k3I, k3J, k3K, k3L}
+
+//}
+
+// clang-format on
