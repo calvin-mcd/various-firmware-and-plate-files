dab0837978ddd051c225ba2a5d73981a7bdf960f
calvin-mcd <114294748+calvin-mcd@users.noreply.github.com>
2026-04-10 11:13:45 +0200

    cleanup

--- firmware-work in progress/elixir/keymaps/default/keymap.c
+++ firmware-work in progress/elixir/keymaps/default/keymap.c
@@ -2,7 +2,7 @@
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-    [0] = { ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN), ENCODER_CCW_CW(KC_PGUP, KC_PGDN) },
+    [0] = { ENCODER_CCW_CW(KC_PGUP, KC_PGDN), ENCODER_CCW_CW(KC_AUDIO_VOL_UP, KC_AUDIO_VOL_DOWN) },
     [1] = { ENCODER_CCW_CW(KC_NO, KC_NO), ENCODER_CCW_CW(KC_NO, KC_NO) },
     [2] = { ENCODER_CCW_CW(KC_NO, KC_NO), ENCODER_CCW_CW(KC_NO, KC_NO) },
     [3] = { ENCODER_CCW_CW(KC_NO, KC_NO), ENCODER_CCW_CW(KC_NO, KC_NO) },
@@ -11,13 +11,48 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
 };
 #endif
 
+#include "rgblight.h" // Indicator Lights
+
+void keyboard_post_init_user(void) {
+    rgblight_enable_noeeprom(); // Enable RGB, without saving settings to EEPROM
+    rgblight_sethsv_noeeprom(128, 255, 255); // Set default color (blue)
+    rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
+}
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    switch (get_highest_layer(state)) {
+        case 0:
+            rgblight_sethsv_noeeprom(128, 255, 255); // Blue
+            break;
+        case 1:
+            rgblight_sethsv_noeeprom(30, 255, 255);  // Amber
+            break;
+        case 2:
+            rgblight_sethsv_noeeprom(85, 255, 255);  // Green
+            break;
+        case 3:
+            rgblight_sethsv_noeeprom(0, 255, 255);    // Red
+            break;
+        case 4:
+            rgblight_sethsv_noeeprom(0, 0, 255);    // White
+            break;
+        case 5:
+            rgblight_sethsv_noeeprom(213, 255, 255);    // Pink
+            break;
+        default:
+            rgblight_sethsv_noeeprom(128, 255, 255); // Default to blue
+            break;
+    }
+    return state;
+}
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO
+        KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_F1,   KC_F2,   KC_F3,   KC_MUTE,
+        KC_ESC,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_DEL,
+        KC_TAB,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,   KC_ENTER,
+        KC_LSFT,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMMA,   KC_DOT,   KC_SLSH,   KC_RSFT,
+        KC_LCTL,   KC_LGUI,   KC_LALT,   KC_DOWN,   KC_BSPC,   KC_SPACE,   KC_UP,   KC_RALT,   KC_MENU,   KC_RCTL
     ),
     [1] = LAYOUT(
         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
